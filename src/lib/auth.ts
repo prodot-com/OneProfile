8934e64dfa249aa001c20b0ed7aba5f5dac0dc0b
Probal Ghosh <probalghoshofficial@gmail.com>
2026-08-27 01:59:32 +0530

    feat: integrate better-auth for authentication and add user model migration

---
+++ src/lib/auth.ts
@@ -0,0 +1,27 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "@/lib/prisma";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+
+  baseURL: process.env.BETTER_AUTH_URL,
+
+  emailAndPassword: {
+    enabled: true,
+  },
+
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    },
+  },
+});
+
+console.log({
+  id: process.env.GITHUB_CLIENT_ID,
+  secretLoaded: !!process.env.GITHUB_CLIENT_SECRET,
+});
